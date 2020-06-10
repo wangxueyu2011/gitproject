b59aa22d133ac9bd8e92db376733e62a23cc34b1
wangxueyu2011 <wangxueyu2011@163.com>
2020-06-10 21:52:00 +0800

    ex

---
+++ ex10.py
@@ -0,0 +1,27 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do  a list
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+clock = "\a \a\a \a\a\a \a\a\a\a"
+print(clock)
+
+hex = "\u12345"
+print(hex)
+
+name = f"\nwangxueyu"
+name1 = f"Hu\tTianjing\r\n"
+
+sum = name+name1
+print(sum)
